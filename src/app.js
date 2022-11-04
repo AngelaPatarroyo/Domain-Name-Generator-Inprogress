294d40b623a2306ca32c898e7585a68004843b88
melbynrojo <angela.patarroyo@hotmail.com>
2022-11-04 10:56:22 +0000

    initial project

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+  document.getElementById("bdominio").addEventListener("click", dominios);
+  dominios();
+};
+
+let dominios = () => {
+  let pronoun = ["the", "our", "my"];
+  let adj = ["lovely", "nice", "fit", "amazing"];
+  let noun = ["cat", "dog", "racoon"];
+  let extensions = [".net", ".com", ".ol", ".it", ".us"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        for (let c = 0; c < extensions.length; c++) {
+          dominios.push(
+            pronoun[i] + adj[a] + noun[b] + extensions[c] + "<br/>"
+          );
+        }
+      }
+    }
+  }
 };
