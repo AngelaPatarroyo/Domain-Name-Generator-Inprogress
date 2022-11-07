03d8d9356a9ce9ccb5491482d4f99a396888445d
melbynrojo <angela.patarroyo@hotmail.com>
2022-11-07 18:13:52 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   console.log("Hello Rigo from the console!");
-  document.getElementById("bdominio").addEventListener("click", dominios);
-  dominios();
+  document.getElementById("bdominio").addEventListener("click", () => {
+    document.querySelector("#dominio1").innerHTML = dominios();
+  });
 };
 
 let dominios = () => {
@@ -17,15 +18,21 @@ let dominios = () => {
   let noun = ["cat", "dog", "racoon"];
   let extensions = [".net", ".com", ".ol", ".it", ".us"];
 
+  let dominios2 = [];
   for (let i = 0; i < pronoun.length; i++) {
     for (let a = 0; a < adj.length; a++) {
       for (let b = 0; b < noun.length; b++) {
         for (let c = 0; c < extensions.length; c++) {
-          dominios.push(
-            pronoun[i] + adj[a] + noun[b] + extensions[c] + "<br/>"
+          dominios2.push(
+            pronoun[i] +
+              adj[a] +
+              noun[b] +
+              extensions[c] +
+              "<br/>" /* ['cualquier cosa', 'otro elemento'] */
           );
         }
       }
     }
   }
+  return dominios2;
 };
